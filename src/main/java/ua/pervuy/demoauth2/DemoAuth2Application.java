d85e737eb61a2bd8bc7544fcae71694a8526c3e0
Aex <sashaep61@gmail.com>
2023-12-18 17:57:59 +0200

    - checkstyle

--- src/main/java/ua/pervuy/demoauth2/DemoAuth2Application.java
+++ src/main/java/ua/pervuy/demoauth2/DemoAuth2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoAuth2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoAuth2Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoAuth2Application.class, args);
+  }
 
 }
